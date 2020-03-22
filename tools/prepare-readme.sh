c3e6acecbd5d0055715323b0cfcf08e1b7546ec6
Andres Kovalev <akovalev@guidewire.com>
2020-03-22 18:45:59 +0100

    chore: fix cicd pipepline

--- tools/prepare-readme.sh
+++ tools/prepare-readme.sh
@@ -4,7 +4,7 @@ set -e
 
 node ./tools/generate-readmes.js ./src/streams/
 node ./tools/generate-readmes.js ./src/utility/
-node ./tools/generate-docs.js ./src/streams/README.hbs --streams ./src/streams > ./src/streams/README.md
-node ./tools/generate-docs.js ./src/utility/README.hbs --utility ./src/utility > ./src/utility/README.md
-node ./tools/generate-docs.js ./README.hbs --streams ./src/streams --utility ./src/utility > ./README.md
-node ./tools/generate-docs.js ./SUMMARY.hbs --streams ./src/streams --utility ./src/utility > ./SUMMARY.md
+node ./tools/generate-docs.js ./templates/streams.hbs --streams ./src/streams > ./src/streams/README.md
+node ./tools/generate-docs.js ./templates/utility.hbs --utility ./src/utility > ./src/utility/README.md
+node ./tools/generate-docs.js ./templates/index.hbs --streams ./src/streams --utility ./src/utility > ./README.md
+node ./tools/generate-docs.js ./templates/summary.hbs --streams ./src/streams --utility ./src/utility > ./SUMMARY.md
